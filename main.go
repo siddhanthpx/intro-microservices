e5670a94f544bb18a669de4307ce6fd34460b699
siddhanthpx <siddhanth0@outlook.com>
2021-03-17 01:15:55 +0530

    added graceful shutdown and custom server settings

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"intro-microservices/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
+
+const TIMEOUT = 30 * time.Second
 
 func main() {
 	l := log.New(os.Stdout, "intro", log.LstdFlags)
@@ -16,5 +21,30 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	toc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	s.Shutdown(toc)
 }
